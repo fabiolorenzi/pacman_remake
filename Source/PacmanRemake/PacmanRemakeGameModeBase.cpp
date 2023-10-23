0dfc66846179cd578839c2993ae064a1375f1a8c
fabiolorenzi <fabiolorenzi13975@gmail.com>
2023-10-23 15:05:42 +0100

    first commit

---
+++ Source/PacmanRemake/PacmanRemakeGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "PacmanRemakeGameModeBase.h"
+
---
+++ Source/PacmanRemake/PacmanRemakeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PacmanRemakeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PACMANREMAKE_API APacmanRemakeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
