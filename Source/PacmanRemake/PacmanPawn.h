cf62b45ace3c0011d6f4feb3b189dbb3494c2da4
fabiolorenzi <fabiolorenzi13975@gmail.com>
2023-10-26 08:43:31 +0100

    added basic pacman class and game mode

---
+++ Source/PacmanRemake/PacmanPawn.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PacmanPawn.generated.h"
+
+UCLASS()
+class PACMANREMAKE_API APacmanPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APacmanPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool Frozen = true;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void SetDirection(const FVector Direction);
+	bool IsFrozen() { return Frozen; }
+
+	UFUNCTION(BlueprintCallable)
+		void SetFrozen(bool Value) { Frozen = Value; }
+
+private:
+	UFUNCTION()
+		void OnOverlapBegin(AActor* PlayerActor, AActor* OtherActor);
+};
